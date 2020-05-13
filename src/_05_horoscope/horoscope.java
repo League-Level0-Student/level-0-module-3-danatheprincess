08bec9da84b5f761e362f15a87e467c867bc9480
root <root@94240d47e9a5>
2020-05-13 01:22:57 +0000

    Working on horoscope

---
+++ src/_05_horoscope/horoscope.java
@@ -0,0 +1,38 @@
+package _05_horoscope;
+
+import javax.swing.JOptionPane;
+
+public class horoscope {
+	public static void main(String arg[]) {
+
+		String sign = JOptionPane.showInputDialog("What is your zodiac sign");
+		if (sign.equals("Libra")) {
+			JOptionPane.showMessageDialog(null,
+					"you have a sense of fair play and become completely upset when things are fair and unjust");
+		}
+		else if(sign.equals("Cancer")){
+			JOptionPane.showMessageDialog(null,"you have an offbeat sense of humor, often finding something humorous that others don’t.");
+		}else if(){
+			
+		}else if(){
+			
+		}else if(){
+			
+		}else if(){
+			
+		}else if(){
+			
+		}else if(){
+			
+		}else if(){
+			
+		}else if(){
+			
+		}else if(){
+			
+		}
+		else{
+		}
+			
+	}
+}
