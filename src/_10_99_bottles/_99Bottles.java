b476492b2cdc29f8f9bcc9b045486490f802729a
root <root@ed637ba260c4>
2020-06-10 01:24:02 +0000

    finished 99Bottles

---
+++ src/_10_99_bottles/_99Bottles.java
@@ -0,0 +1,28 @@
+package _10_99_bottles;
+
+public class _99Bottles {
+public static void main(String[] args) {
+	for(int b=99;  b>1;b-- ) {
+		System.out.println(b+ " bottles of beer on the wall, " +b+ "  bottles of beer.");
+		System.out.println("Take one down and pass it around, " +(b-1) + " bottles of beer on the wall.");
+
+	}
+	
+	System.out.println("1 bottles of beer on the wall, 1 bottles of beer.");
+	System.out.println("Take one down and pass it around, no more bottles of beer on the wall.");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
